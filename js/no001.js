1a95b845da19067b8e3c734eea4f5e604eb92b36
Dan Hermes <dan@counsyl.com>
2011-03-09 23:40:31 -0500

    Added some more questions and began to clean up bad variable names, bad python practices.

---
+++ js/no001.js
@@ -0,0 +1,48 @@
+<script type="text/javascript">
+/* If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000. */
+function all_multiples(bound, base) {
+  var result = [];
+  for(var i = 0; base*(i + 1) < bound; i++) {
+    result[i] = base*(i + 1);
+  }
+  return result;
+}
+
+function unique(arr) {
+  var hash = {}
+  for(var i = 0, val; val = arr[i]; i++) {
+    if (hash.hasOwnProperty(val)) {
+      hash[val] += 1;
+    } else {
+      hash[val] = 1;
+    }
+  }
+
+  result = [];
+  for(var key in hash) {
+    result.push(parseInt(key));
+  }
+  return result;
+}
+
+function sum(arr) {
+  //not robust, expects integers or strings
+  result = 0;
+  for(var i = 0, val; val = arr[i]; i++) {
+    result += val;
+  }
+  return result;
+}
+
+function main() {
+  both_sets = all_multiples(1000, 3).concat(all_multiples(1000, 5));
+  uniq_mults = unique(both_sets);
+  final = sum(uniq_mults);
+  alert("The answer to Euler Project, question 1 is: " + final);
+}
+
+main();
+</script>
--- python_code/functions.py
+++ python_code/functions.py
@@ -1,7 +1,16 @@
 from os import path as opath
 import sys
 
+# 8, 11, 13, 18, 22, 42, 54, 59, 67
 def get_data(problem_number):
+    """
+    Takes a problem number and returns the raw text
+    from the expected data file
+
+    Data is expected to be in euler_project/problem_data and
+    to be named no---.txt where --- is the zero padded
+    problem number
+    """
     path = opath.abspath(opath.dirname(sys.argv[0]))
 
     relative_filename = 'no%s.txt' % str(problem_number).zfill(3)
@@ -12,6 +21,61 @@ def get_data(problem_number):
 
     filename = opath.join(path, relative_path)
     with open(filename) as fh:
-        # files if file doesn't exist
+        # fails if file doesn't exist
         result = fh.read()
     return result
+
+# 18, 67
+def max_sum(triangle_matrix):
+    """
+    Finds maximum sum of path from top of triangle down to bottom
+
+    Input: Matrix of triangle e.g.
+      1
+     3 5
+    7 8 4
+    becomes [[1], [3, 5], [7, 8, 4]]
+    Uses memoization from the bottom layer up to work quickly
+    Output: maximum value
+    """
+    max_depth = len(triangle_matrix) - 1
+
+    # Set bottom row for memoization
+    depth = max_depth
+    result = {}
+    for i, entry in enumerate(triangle_matrix[depth]):
+        result[(i, max_depth - depth)] = entry
+    depth -= 1
+
+    # Set each row moving up the triangle based on
+    # the results one low below
+    while depth >= 0:
+        for i, entry in enumerate(triangle_matrix[depth]):
+            result[(i, max_depth - depth)] = entry + max(
+                result[(i, max_depth - depth - 1)], result[(i + 1, max_depth - depth - 1)] )
+        depth -= 1
+
+    return result[(0, max_depth - depth - 1)]
+
+# 21, 27, 37
+def sieve(n):
+    """
+    Sieve of Eratosthenes
+
+    Returns all primes <= n
+    """
+    to_check = [1] * (n+1)
+    for i in range(2,n+1):
+        if to_check[i]:
+            for j in range(2*i,n+1,i):
+                to_check[j] = 0
+
+    return [ i for i in range(2,n+1) if to_check[i] ]
+
+# 25
+def fibonacci_generator():
+    """a generator for Fibonacci numbers"""
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b

042ac8429533b42c6c2e257f0b05b7274afd8f96
Daniel Hermes <daniel.j.hermes@gmail.com>
2011-08-19 10:43:38 -0700

    Fixed confused with store_false

--- python_code/benchmark.py
+++ python_code/benchmark.py
@@ -7,17 +7,17 @@ import time
 parser = argparse.ArgumentParser()
 parser.add_argument('--iterations', metavar='N', type=int,
                     help='number of times to run the benchmark')
-parser.add_argument('--sum', action='store_false',
+parser.add_argument('--sum', action='store_true',
                     help='sum the times (default: find the average)')
-parser.add_argument('--fast', action='store_false',
+parser.add_argument('--fast', action='store_true',
                     help='just execute the short running problems')
 args = parser.parse_args()
 iterations = 1 if args.iterations is None else args.iterations
 
 if args.fast:
-  problem_list = range(1, 50 + 1)
-else:
   problem_list = [1, 2, 3, 5, 6, 8, 13, 15, 16, 18, 19, 20, 24, 28, 40, 45]
+else:
+  problem_list = range(1, 50 + 1)
 
 for problem_number in problem_list:
   path = 'complete/no%03d.py' % problem_number

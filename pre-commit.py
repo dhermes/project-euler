f71c578b32c8ff9e6d5d0f00d4601da5eb79cc9a
Daniel Hermes <daniel.j.hermes@gmail.com>
2011-10-05 15:04:24 -0700

    Updated pre-commit hook

--- pre-commit.py
+++ pre-commit.py
@@ -22,3 +22,24 @@ with open(confidential_config, 'w') as fh:
   fh.write(git_included_contents)
 
 os.system('git add %s' % git_included_config)
+
+# In the rare case, that config.js is the only file changed,
+# this switch will render the commit worthless (since it will
+# move the repo into an unchanged state. In this case, we
+# revert the changes before exiting.
+
+with os.popen('git st') as fh:
+  git_status = fh.read()
+
+if ('nothing to commit' in git_status or
+    'no changes added to commit' in git_status):
+  msg = '# From pre-commit hook: No commit necessary, ' \
+        'sensitive config unchanged. #'
+  hash_head = '#' * len(msg)
+  print ('%s\n%s\n%s\n\n' % (hash_head, msg, hash_head)),
+
+  with open(git_included_config, 'w') as fh:
+    fh.write(git_included_contents)
+
+  with open(confidential_config, 'w') as fh:
+    fh.write(confidential_contents)

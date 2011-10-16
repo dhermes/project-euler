27faaf036f4a944cbb0cfe871d7f42897aa2f710
Daniel Hermes <daniel.j.hermes@gmail.com>
2011-10-16 11:16:20 -0700

    Fixed accident write of no008 to no010

---
+++ java/complete/no008.java
@@ -0,0 +1,75 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+class no008 {
+  public static String get_data() {
+    String contents = "";
+    try {
+      String data_path = "/Users/dhermes/dhermes-project-euler/problem_data/";
+      FileReader input = new FileReader(data_path + "no008.txt");
+      BufferedReader buffered_input =  new BufferedReader(input);
+      try {
+        String line;
+        while ((line = buffered_input.readLine()) != null) {
+          contents += line;
+          contents += System.getProperty("line.separator");
+        }
+      } finally {
+        buffered_input.close();
+      }
+    } catch (IOException exc) {
+      exc.printStackTrace();
+    }
+
+    return contents;
+  }
+
+  public static String join(String join_val, String[] arr) {
+    String result = "";
+    int length = arr.length;
+    for (int i = 0; i < length - 1; i++) {
+      result += arr[i] + join_val;
+    }
+
+    if (length > 0) {
+      result += arr[length - 1];
+    }
+
+    return result;
+  }
+
+  public static int product_consec_digits(String number, int consecutive) {
+    int length = number.length();
+    int[] digits = new int[length];
+    for (int i = 0; i < length; i++) {
+      digits[i] = Integer.parseInt(Character.toString(number.charAt(i)));
+    }
+
+    int ans = 0;
+    int product;
+    int max_start = length - consecutive;
+    for (int i = 0; i <= max_start; i++) {
+      product = 1;
+      for (int j = 0; j < consecutive; j++) {
+        product *= digits[i + j];
+      }
+      ans = Math.max(ans, product);
+    }    
+
+    return ans;
+  }
+
+  public static int main(boolean verbose) {
+    String contents = join("", get_data().split("\n"));
+    return product_consec_digits(contents, 5);
+  }
+
+  public static long main() {
+    return main(false);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(main());
+  }
+}
--- java/complete/no010.java
+++ java/complete/no010.java
@@ -1,68 +1,43 @@
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.util.Arrays;
 
 class no010 {
-  public static String get_data() {
-    String contents = "";
-    try {
-      String data_path = "/Users/dhermes/dhermes-project-euler/problem_data/";
-      FileReader input = new FileReader(data_path + "no008.txt");
-      BufferedReader buffered_input =  new BufferedReader(input);
-      try {
-        String line;
-        while ((line = buffered_input.readLine()) != null) {
-          contents += line;
-          contents += System.getProperty("line.separator");
+  public static int [] sieve(int n) {
+    boolean[] to_check = new boolean[n + 1];
+    Arrays.fill(to_check, true);
+    int final_check = (int) Math.sqrt(n);
+
+    for (int i = 2; i <= final_check; i++) {
+      if (to_check[i]) {
+        for (int j = i * i; j <= n; j += i) {
+          to_check[j] = false;
         }
-      } finally {
-        buffered_input.close();
       }
-    } catch (IOException exc) {
-      exc.printStackTrace();
     }
 
-    return contents;
-  }
-
-  public static String join(String join_val, String[] arr) {
-    String result = "";
-    int length = arr.length;
-    for (int i = 0; i < length - 1; i++) {
-      result += arr[i] + join_val;
+    int[] result = new int[n];
+    int curr_index = 0;
+    for (int i = 2; i <= n; i++) {
+      if (to_check[i]) {
+        result[curr_index] = i;
+        curr_index++;
+      }
     }
 
-    if (length > 0) {
-      result += arr[length - 1];
+    if (curr_index != n) {
+      result = Arrays.copyOfRange(result, 0, curr_index);
     }
-
     return result;
   }
 
-  public static int product_consec_digits(String number, int consecutive) {
-    int length = number.length();
-    int[] digits = new int[length];
-    for (int i = 0; i < length; i++) {
-      digits[i] = Integer.parseInt(Character.toString(number.charAt(i)));
+  public static long main(boolean verbose) {
+    int[] primes = sieve(2000000 - 1);
+
+    long result = 0;
+    for (int i = 0; i < primes.length; i++) {
+      result += primes[i];
     }
 
-    int ans = 0;
-    int product;
-    int max_start = length - consecutive;
-    for (int i = 0; i <= max_start; i++) {
-      product = 1;
-      for (int j = 0; j < consecutive; j++) {
-        product *= digits[i + j];
-      }
-      ans = Math.max(ans, product);
-    }    
-
-    return ans;
-  }
-
-  public static int main(boolean verbose) {
-    String contents = join("", get_data().split("\n"));
-    return product_consec_digits(contents, 5);
+    return result;
   }
 
   public static long main() {
